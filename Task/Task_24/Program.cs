08f8cb7cbc3ff27782ee72034fa7f2ceae88d683
marina <simashmarinka@gmail.com>
2022-09-26 16:58:17 +0500

    Task_24

---
+++ Task/Task_24/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// например: 7 -> 28     4 -> 10    8 -> 36  
+
+
+
+/* Console.Write("Введите натуральное число ");
+int num = int.Parse(Console.ReadLine());
+
+int GetSumNumbers(int number)                               // метод int  который что то возвращает
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
+
+int sumResult = GetSumNumbers (num);
+Console.WriteLine($"Сумма чисел от 1 до {num} = {sumResult}"); */
+
+
+// не корректный метод который не рекомендуется использовать
+Console.Write("Введите натуральное число ");
+int num = int.Parse(Console.ReadLine());
+
+void GetSumNumbers(int number)                              // метод void  который не возвращает ничего
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum = sum + i;
+    }
+    Console.WriteLine($"Сумма чисел от 1 до {num} = {sum}");
+}
+
+GetSumNumbers(num);
