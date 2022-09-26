fb498f81f44750fa087ef45cd938e870ee64b7d1
marina <simashmarinka@gmail.com>
2022-09-26 16:55:12 +0500

    Task_22

---
+++ Task/Task_22/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int number = 1;
+while (number <= num)
+{
+    Console.WriteLine($"Число {number,5} | Квадрат {number * number,5}");
+    number++;
+}
+Console.WriteLine("Конец");
