6cf5d8a75848455e360e80ae01bc60bd31590615
marina <simashmarinka@gmail.com>
2022-09-26 18:14:13 +0500

    Task_63

---
+++ Task/Task_63/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задайте значение N. Напишите программу, которая выведет
+все натуральные числа в промежутке от 1 до N. */
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+void Num(int num)
+{
+    if (num == 0) return;
+    Num(num - 1);
+    Console.Write($"{num} ");
+}
+
+Num(num);
