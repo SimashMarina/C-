7b72ebf57bf299842122d66d4c570b0ee23ed5a0
marina <simashmarinka@gmail.com>
2022-09-26 16:59:36 +0500

    Task_25_dz

---
+++ Task/Task_25_dz/Program.cs
@@ -0,0 +1,33 @@
+﻿// // Напишите цикл, который принимает на вход
+// два числа (A и B) и возводит число A в натуральную
+// степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+/* Console.WriteLine("Введите первое число");
+double num = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+double num2 = double.Parse(Console.ReadLine());
+
+double result = Math.Pow(num, num2);
+Console.WriteLine(result); */
+
+// Решение с помощью метода
+
+int Expon(int numberA, int numberB)
+{
+    int res = 1;
+    for (int i = 1; i <= numberB; i++)
+    {
+        res = res * numberA;
+    }
+    return res;
+}
+
+Console.Write("Введите число A: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = int.Parse(Console.ReadLine());
+
+int expon1 = Expon(numberA, numberB);
+Console.WriteLine($"{numberA} в степени {numberB} = {expon1}");
