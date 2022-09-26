6de5105be2751631c64be70a3f652f2cfd242eb0
marina <simashmarinka@gmail.com>
2022-09-26 18:19:40 +0500

    Task_67_dz

---
+++ Task/Task_67/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Напишите программу, которая будет принимать на вход число и
+возвращать сумму его цифр.
+453 -> 12
+45 -> 9 */
+
+Console.WriteLine("Введите натуральное число");
+int n = int.Parse(Console.ReadLine());
+
+GetSum(n, 0);
+
+void GetSum(int num, int sum)
+{
+    if (num == 0)
+    {
+        Console.WriteLine(sum);
+        return;
+    }
+
+    sum = sum + num % 10;
+    num = num / 10;
+    GetSum(num, sum);
+}
